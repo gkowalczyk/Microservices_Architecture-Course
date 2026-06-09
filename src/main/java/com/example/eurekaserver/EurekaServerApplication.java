74089597c16df4fb637478c1a68336445e2e8e56
Grzegorz Kowalczyk <grzegorzkowalczyk1989@gmail.com>
2026-06-09 06:26:41 +0200

    added database connection configuration

--- src/main/java/com/example/eurekaserver/EurekaServerApplication.java
+++ src/main/java/com/example/eurekaserver/EurekaServerApplication.java
@@ -1,12 +1,20 @@
 package com.example.eurekaserver;
 
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+import org.springframework.context.annotation.Bean;
+import org.springframework.core.env.Environment;
 
 @SpringBootApplication
 @EnableEurekaServer
 public class EurekaServerApplication {
+
+    @Bean
+    CommandLineRunner runner(Environment env) {
+        return args -> System.out.println("PORT = " + env.getProperty("server.port"));
+    }
 
     public static void main(String[] args) {
         SpringApplication.run(EurekaServerApplication.class, args);
